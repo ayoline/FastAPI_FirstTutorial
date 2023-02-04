de613122cc9a178a44b9dc7f903eaefa40488b24
Anderson P Soares <ayo@outlook.com.br>
2023-02-04 16:20:34 -0300

    add new POST route

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ from pydantic import BaseModel
 
 class Item(BaseModel):
     id: int
+    decricao: str
+    valor: float
 
 
 app = FastAPI()
@@ -18,3 +20,8 @@ def read_root():
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
+
+
+@app.post("/item")
+def add_item(novo_item: Item, outro_item: Item):
+    return [novo_item, outro_item]
