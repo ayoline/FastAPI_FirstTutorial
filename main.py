00db1a46e74fa254995253927191680f3cfc7649
Anderson P Soares <ayo@outlook.com.br>
2023-02-05 16:43:58 -0300

    finish this example

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from typing import Union
-from fastapi import FastAPI
+from typing import Union, Optional
+from fastapi import FastAPI, Header, Response, Cookie
 from pydantic import BaseModel
 
 
@@ -13,8 +13,19 @@ app = FastAPI()
 
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+def read_root(user: Optional[str] = Header("123")):
+    return {"user-agent": user}
+
+
+@app.get("/cookie")
+def cookie(response: Response):
+    response.set_cookie(key="meu_cookie", value="12345678")
+    return {"cookie": True}
+
+
+@app.get("/get-cookie")
+def get_cookie(meu_cookie: Optional[str] = Cookie(None)):
+    return {"Cookie": meu_cookie}
 
 
 @app.get("/items/{item_id}")
